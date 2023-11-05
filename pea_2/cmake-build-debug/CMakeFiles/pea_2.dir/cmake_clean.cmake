4c7bde3cd49042048fb65607adea4d5ab6300c85
Jacek <stuff.jackozbogdanca@gmail.com>
2023-11-05 23:14:29 +0100

    add files

---
+++ pea_2/cmake-build-debug/CMakeFiles/pea_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pea_2.dir/main.cpp.obj"
+  "CMakeFiles/pea_2.dir/main.cpp.obj.d"
+  "libpea_2.dll.a"
+  "pea_2.exe"
+  "pea_2.exe.manifest"
+  "pea_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pea_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
